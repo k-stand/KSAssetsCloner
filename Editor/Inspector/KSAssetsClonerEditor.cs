024fdfa4f253bacefdb001c9cd6293ea73d1fbf0
KiriumeStand <198204454+KiriumeStand@users.noreply.github.com>
2026-02-02 14:06:46 +0900

    ライセンス変更

--- Editor/Inspector/KSAssetsClonerEditor.cs
+++ Editor/Inspector/KSAssetsClonerEditor.cs
@@ -1,4 +1,30 @@
-// 参考 : https://github.com/Narazaka/CopyAssetsWithDependency/blob/main/CopyAssetsWithDependency.cs
+/*
+Portions of this code are based on https://qiita.com/k7a/items/eb5a3ee4ed6448343543 by k7a
+and https://github.com/Narazaka/CopyAssetsWithDependency by Narazaka.
+*/
+/*
+Copyright (c) 2020 Narazaka
+Copyright (c) 2026 KiriumeStand
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+   1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+
+   2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+   3. This notice may not be removed or altered from any source
+   distribution.
+*/
 
 using System;
 using System.Collections.Generic;
@@ -219,7 +245,6 @@ namespace io.github.kiriumestand.ksassetscloner.editor
         {
             return Uri.UnescapeDataString(new Uri(fromPath).MakeRelativeUri(new Uri(toPath)).ToString());
         }
-
 
         public static T Bind<T>(
             VisualElement root,
